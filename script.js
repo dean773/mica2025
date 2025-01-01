8376dbe660fbdd11597a45869c7c8041a0ea400c
Dean Sphikas <deansensei64@gmail.com>
2025-01-01 17:22:24 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// add the name of each image title here
+const imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg","1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
+const sliderItems = imageNames.concat(imageNames);
+const sliders = document.querySelectorAll(".slide-track");
+
+sliders.forEach((slider, index) => {
+  slider.innerHTML = ""; // Kosongkan isi slider sebelum menambahkan gambar
+
+  sliderItems.forEach((sliderItem) => {
+    const img = document.createElement("img");
+    img.src = "./assets/" + sliderItem; //path to your folder image
+    img.className = "slide-item";
+    slider.appendChild(img);
+  });
+});
+
+// Use the code below if you only want to create one slider
+
+/*
+  const sliderItems = imageNames.concat(imageNames);
+
+  const slider = document.getElementById("slide-track");
+
+  sliderItems.forEach((sliderItem) => {
+    const img = document.createElement("img");
+    img.src = "./assets/" + sliderItem; //path to your folder image
+    img.className = "slide-item";
+    slider.appendChild(img);
+  })*/
